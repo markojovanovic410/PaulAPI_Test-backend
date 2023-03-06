e9f2ea501010f3116e01f22d5d53df7834ca2e5a
markojovanovic410 <markojovfreelancer@gmail.com>
2023-03-06 13:30:42 +0100

    proxy-test-2

--- src/api.js
+++ src/api.js
@@ -1,5 +1,11 @@
-const express = require('express')
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
+const router = express.Router();
+const bodyParser = require('body-parser');
+const axios = require("axios");
 const { createProxyMiddleware } = require('http-proxy-middleware')
+
 const cors= require('cors');
 
 const app = express()
@@ -9,10 +15,16 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST"]
 }));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);

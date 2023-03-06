f9230f79edbdb6d235f92fd07f938b83077f273e
markojovanovic410 <markojovfreelancer@gmail.com>
2023-03-06 11:52:08 +0100

    Add: response header

--- src/api.js
+++ src/api.js
@@ -14,6 +14,12 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const cors=require("cors");
 const corsOptions ={
    origin:'*', 

eb4cbe77e3377ec3b0bca4f2b87701208f6ff657
markojovanovic410 <markojovfreelancer@gmail.com>
2023-03-07 06:24:04 +0100

    last version

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const cookie = require("cookie");
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 axios.defaults.withCredentials = true;
-const CircularJSON = require('circular-json');
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'https://paul.blueboxonline.com';
-
-
 
 app.post("/login", async (req, res) => {
     try {
@@ -50,6 +45,7 @@ app.post("/session", async(req, res) => {
 app.post("/table", async(req, res) => {
     try {
         const cookie = req.body.cookie
+        console.log(cookie)
         let response = await axios.get("https://paul.blueboxonline.com/api/v1/app/tabledata",{
             headers: {
               'Cookie': cookie
@@ -83,14 +79,11 @@ app.post("/report", async(req, res) => {
     try {
         const cookie = req.body.cookie;
         let reportData = req.body.reportData;
-        console.log(cookie)
-        console.log(reportData)
         let response = await axios.post("https://paul.blueboxonline.com/api/v1/app/report", reportData, {
             headers: {
               'Cookie': cookie
             }
         })
-         console.log(response)
          res.json({success: true, message: response.data})
         } catch(e){
             console.log(e);
